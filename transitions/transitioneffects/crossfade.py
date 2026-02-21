3207b291747204e9558f3982f281f6a3c3ce7ac2
siddishczar <70487945+siddishczar@users.noreply.github.com>
2026-02-21 17:19:35 -0500

    Update crossfade.py
    Made it so that it takes in fade in/out times rather than comes up with it

--- transitions/transitioneffects/crossfade.py
+++ transitions/transitioneffects/crossfade.py
@@ -4,7 +4,15 @@ import numpy as np
 import os
 from sections import get_sections
 
-def crossfade(wav1_path, wav2_path, output_path, fade_duration=8):
+def crossfade(wav1_path, wav2_path, output_path, fade_out_start, fade_in_end):
+    """
+    Args:
+        wav1_path:      first song
+        wav2_path:      second song
+        output_path:    where to save
+        fade_out_start: timestamp (in seconds) where song 1 starts fading out
+        fade_in_end:    timestamp (in seconds) where song 2 finishes fading in (i.e. the beat drop)
+    """
     y1, sr1 = librosa.load(wav1_path, sr=None, mono=False)
     y2, sr2 = librosa.load(wav2_path, sr=None, mono=False)
 
@@ -12,19 +20,13 @@ def crossfade(wav1_path, wav2_path, output_path, fade_duration=8):
         y2 = librosa.resample(y2, orig_sr=sr2, target_sr=sr1)
     sr = sr1
 
-    moments1 = dict(get_sections(wav1_path))  #{"Intro": "0:00.0", "Outro": "3:00.0", ...}
-    moments2 = dict(get_sections(wav2_path))
-
-    fade_out_start = parse_time(moments1.get('Outro') or moments1.get('Chorus'))
-    fade_in_start  = parse_time(moments2.get('Buildup') or moments2.get('Intro'))
-
     fade_out_sample = int(fade_out_start * sr)
-    fade_in_sample  = int(fade_in_start * sr)
+    fade_in_sample  = int(fade_in_end * sr)
 
     fade_out_len = len(y1[0]) - fade_out_sample
     fade_in_len  = fade_in_sample
 
-    fade_out_curve = np.linspace(1, 0, fade_out_len) #change later if we do not want it to be linear
+    fade_out_curve = np.linspace(1, 0, fade_out_len)
     fade_in_curve  = np.linspace(0, 1, fade_in_len)
 
     y1[..., fade_out_sample:] *= fade_out_curve
@@ -42,11 +44,3 @@ def crossfade(wav1_path, wav2_path, output_path, fade_duration=8):
     output = output / np.max(np.abs(output))
     sf.write(output_path, output.T, sr)
     print(f"Saved crossfade to: {output_path}")
-
-
-def parse_time(timestamp):
-    #converts to seconds
-    if timestamp is None:
-        return 0
-    m, s = timestamp.split(':')
-    return int(m) * 60 + float(s)
